a3aca7fba60fa6082dcd40d84b28841e981b16c3
miaow1 <mw3302@columbia.edu>
2019-10-31 14:39:30 -0400

    hw8

--- fms_bootstrap/fms_instrument_fra.h
+++ fms_bootstrap/fms_instrument_fra.h
@@ -5,3 +5,16 @@
 #pragma once
 
 //!!! Implement the class forward_rate_agreement in namespace fms::instrument.
+#include "fms_instrument_sequence.h"
+
+namespace fms::instrument {
+
+	template<class U = double, class C = double>
+	struct forward_rate_agreement : public sequence<fms::sequence::list<U>, fms::sequence::list<C>> {
+		forward_rate_agreement(U effective, U tenor, C forward)
+			: sequence<fms::sequence::list<U>, fms::sequence::list<C>>(
+				fms::sequence::list<U>({ effective, effective+tenor }), fms::sequence::list<C>({ -1, 1 + forward * tenor })
+				)
+		{ }
+	};
+}
