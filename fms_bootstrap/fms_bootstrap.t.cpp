189bf946cbb0aed3014302231ac23894a37ee6e3
miaow1 <mw3302@columbia.edu>
2019-10-31 19:20:00 -0400

    hw8

--- fms_bootstrap/fms_bootstrap.t.cpp
+++ fms_bootstrap/fms_bootstrap.t.cpp
@@ -1,116 +1,78 @@
-// fms_instrument.t.cpp - Test fms::instrument
+// fms_bootstrap.t.cpp - Test bootstrap algorithm.
 #include <cassert>
 #include "../fms_sequence/fms_sequence.h"
+#include "fms_bootstrap.h"
 #include "fms_instrument.h"
+#include "fms_pwflat.h"
 
-using namespace fms::sequence;
-using namespace fms::instrument;
+using namespace fms::bootstrap;
+using namespace fms::pwflat;
 
-template<class U, class C>
-int test_instrument_sequence()
+using fms::sequence::list;
+using fms::sequence::apply;
+using fms::sequence::sum;
+
+template<class X = double>
+int scratch()
 {
-	sequence s(iota<U>(1), constant<C>(2));
-	assert(s);
-	auto p = std::pair<U, C>(1, 2);
-	assert(p == *s);
-	++s;
-	++p.first;
-	assert(p == *s);
 
-	return 0;
+    return 0;
 }
-int test_instrument_sequence_int_int = test_instrument_sequence<int, int>();
+int scratch_double = scratch<double>();
 
-template<class U, class C>
-int test_instrument_cd()
+int test_bootstrap_extend()
 {
-	cash_deposit<U, C> cd(2, 3);
+	auto t = list<double>{};
+	auto f = list<double>{};
+	forward F(t,f);
+	double _t;
+	double _f;
+	{
+	//	auto cd0 = fms::instrument::sequence(list({ 0., -1. }), list({ 0.25, 1.01 }));
+		auto u0 = list<double>({ 0., 0.25 });
+		auto c0 = list<double>({ -1, 1.01 });
+		auto [u,r] = extend(F, 0., 0., u0, c0);
+		assert(u == 0.25);
+		auto p = 1.01 * exp(-r * 0.25);
+		assert(p == 1);
 
-	auto u = cd.time();
-	assert(0 == *u);
-	++u;
-	assert(2 == *u);
-
-	auto c = cd.cash();
-	assert(-1 == *c);
-	++c;
-	assert(1 + 2 * 3 == *c);
-
-	assert(std::pair(0, -1) == *cd);
-	++cd;
-	assert(std::pair(2, 1 + 2 * 3) == *cd);
-	++cd;
-	assert(!cd);
-
-	return 0;
-}
-int test_instrument_cd_int_int = test_instrument_cd<int, int>();
-
-template<class B, class U, class C>
-int test_instrument_fra()
-{
-	forward_rate_agreement<B, U, C> fra(1, 2, 3);
-
-	//Check times
-	auto u = fra.time();
-	assert(0 == *u);
-	++u;
-	assert(1 == *u);
-	++u;
-	assert(3 == *u);
-
-	//Check cash flows
-	auto c = fra.cash();
-	assert(0 == *c);
-	++c;
-	assert(-1 == *c);
-	++c;
-	assert(7 == *c);
-
-	//Check pairings
-	assert(std::pair(0, 0) == *fra);
-	++fra;
-	assert(std::pair(1, -1) == *fra);
-	++fra;
-	assert(std::pair(3, 7) == *fra);
-	++fra;
-	assert(!fra);
-
-	return 0;
-}
-int test_instrument_fra_int_int = test_instrument_fra<int, int, int>();
-
-template<class U, class C>
-int test_instrument_swap()
-{
-	interest_rate_swap<U, C> swap(1, 2, 3);
-
-	//Check times, cash flows, and pairings
-	auto u = swap.time();
-	auto c = swap.cash();
-	assert(0 == *u);
-	++u;
-	assert(-1 == *c);
-	++c;
-	assert(std::pair(0, -1) == *swap);
-	++swap;
-
-	int i = 1;
-	for (i = 1; i < 2; i++) {
-		assert(i / 2 == *u);
-		++u;
-		assert(3 / 2 == *c);
-		++c;
-		assert(std::pair(i / 2, 3 / 2) == *swap);
-		++swap;
+		_t = u;
+		_f = r;
+	}
+	{
+		auto cd0 = fms::instrument::sequence(list({ 0., 0.25 }), list({ -1., 1.01 }));
+		list<double> u0 = cd0.time();
+		list<double> c0 = cd0.cash();
+		auto [u, r] = extend(F, 0., 0., u0, c0);
+		assert(u == 0.25);
+		auto p = 1.01 * exp(-r * 0.25);
+		assert(p == 1);
+	}
+	t.push_back(_t);
+	f.push_back(_f);
+	{
+		auto cd1 = fms::instrument::sequence(list({ 0., 0.5 }), list({ -1., 1.05 }));
+		auto [u, r] = extend(F, _t, 0., cd1.time(), cd1.cash());
+		assert(u == 0.5);
+		//auto p = 1.01 * exp(-r * 0.25);
+		//assert(p == 1);
 	}
 
-	assert(1 == *u);
-	assert(1 + 3 / 2 == *c);
-	assert(std::pair(1, 1 + 3 / 2) == *swap);
-	++swap;
-	assert(!swap);
-
+	/*
+	C r = 0.01;
+	cd cd1(0.25, r);
+	//...
+	fra fra1(.25, 1, r);
+	//...
+	swap swap1(2, 0.5, r);
+	forward f;
+	f = bootstrap(f, cd1);
+	*/
 	return 0;
 }
-int test_instrument_swap_int_int = test_instrument_swap<int, int>();
+int test_bootstrap_extend_ = test_bootstrap_extend();
+
+int main()
+{
+	return 0;
+}
