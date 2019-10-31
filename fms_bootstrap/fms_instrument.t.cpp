4562c5d1950da6969146f931fe1b8e646554d67f
miaow1 <mw3302@columbia.edu>
2019-10-31 19:19:11 -0400

    hw8

--- fms_bootstrap/fms_instrument.t.cpp
+++ fms_bootstrap/fms_instrument.t.cpp
@@ -45,3 +45,72 @@ int test_instrument_cd()
 	return 0;
 }
 int test_instrument_cd_int_int = test_instrument_cd<int, int>();
+
+template<class B, class U, class C>
+int test_instrument_fra()
+{
+	forward_rate_agreement<B, U, C> fra(1, 2, 3);
+
+	//Check times
+	auto u = fra.time();
+	assert(0 == *u);
+	++u;
+	assert(1 == *u);
+	++u;
+	assert(3 == *u);
+
+	//Check cash flows
+	auto c = fra.cash();
+	assert(0 == *c);
+	++c;
+	assert(-1 == *c);
+	++c;
+	assert(7 == *c);
+
+	//Check pairings
+	assert(std::pair(0, 0) == *fra);
+	++fra;
+	assert(std::pair(1, -1) == *fra);
+	++fra;
+	assert(std::pair(3, 7) == *fra);
+	++fra;
+	assert(!fra);
+
+	return 0;
+}
+int test_instrument_fra_int_int = test_instrument_fra<int, int, int>();
+
+template<class U, class C>
+int test_instrument_swap()
+{
+	interest_rate_swap<U, C> swap(1, 2, 3);
+
+	//Check times, cash flows, and pairings
+	auto u = swap.time();
+	auto c = swap.cash();
+	assert(0 == *u);
+	++u;
+	assert(-1 == *c);
+	++c;
+	assert(std::pair(0, -1) == *swap);
+	++swap;
+
+	int i = 1;
+	for (i = 1; i < 2; i++) {
+		assert(i / 2 == *u);
+		++u;
+		assert(3 / 2 == *c);
+		++c;
+		assert(std::pair(i / 2, 3 / 2) == *swap);
+		++swap;
+	}
+
+	assert(1 == *u);
+	assert(1 + 3 / 2 == *c);
+	assert(std::pair(1, 1 + 3 / 2) == *swap);
+	++swap;
+	assert(!swap);
+
+	return 0;
+}
+int test_instrument_swap_int_int = test_instrument_swap<int, int>();
