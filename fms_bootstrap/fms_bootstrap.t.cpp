ace947b69b5f662a3ce917a02f5433c5da890dda
miaow1 <mw3302@columbia.edu>
2019-10-31 15:01:07 -0400

    hw8

--- fms_bootstrap/fms_bootstrap.t.cpp
+++ fms_bootstrap/fms_bootstrap.t.cpp
@@ -7,19 +7,14 @@
 
 using namespace fms::bootstrap;
 using namespace fms::pwflat;
+using namespace fms::sequence;
+using namespace fms::instrument;
 
 using fms::sequence::list;
 using fms::sequence::apply;
 using fms::sequence::sum;
 
 template<class X = double>
-int scratch()
-{
-
-    return 0;
-}
-int scratch_double = scratch<double>();
-
 int test_bootstrap_extend()
 {
 	auto t = list<double>{};
@@ -73,6 +68,116 @@ int test_bootstrap_extend()
 }
 int test_bootstrap_extend_ = test_bootstrap_extend();
 
+template<class U, class C>
+int test_instrument_sequence()
+{
+	sequence s(iota<U>(1), constant<C>(2));
+	assert(s);
+	auto p = std::pair<U, C>(1, 2);
+	assert(p == *s);
+	++s;
+	++p.first;
+	assert(p == *s);
+
+	return 0;
+}
+int test_instrument_sequence_int_int = test_instrument_sequence<int, int>();
+
+template<class U, class C>
+int test_instrument_cd()
+{
+	cash_deposit<U, C> cd(2, 3);
+
+	auto u = cd.time();
+	assert(0 == *u);
+	++u;
+	assert(2 == *u);
+
+	auto c = cd.cash();
+	assert(-1 == *c);
+	++c;
+	assert(1 + 2 * 3 == *c);
+
+	assert(std::pair(0, -1) == *cd);
+	++cd;
+	assert(std::pair(2, 1 + 2 * 3) == *cd);
+	++cd;
+	assert(!cd);
+
+	return 0;
+}
+int test_instrument_cd_int_int = test_instrument_cd<int, int>();
+
+template<class U, class C>
+int test_instrument_fra()
+{
+	forward_rate_agreement<U, C> fra(1,2, 3);
+
+	auto u = fra.time();
+	assert(1 == *u);
+	++u;
+	assert(3 == *u);
+
+	auto c = fra.cash();
+	assert(-1 == *c);
+	++c;
+	assert(1 + 2 * 3 == *c);
+
+	assert(std::pair(1, -1) == *fra);
+	++fra;
+	assert(std::pair(3, 1 + 2 * 3) == *fra);
+	++fra;
+	assert(!fra);
+
+	return 0;
+}
+int test_instrument_fra_int_int = test_instrument_fra<int, int>();
+
+template<class U, class C,class T>
+int test_instrument_swap()
+{
+	//(maturity, frequency, coupon)
+	interest_rate_swap<U, C, T> swap(1.7, 2, 3);
+
+	auto u = swap.time();
+	assert(0 == *u);
+	++u;	
+	assert(0.5 == *u);
+	++u;
+	assert(1 == *u);
+	++u;
+	assert(1.5 == *u);
+	++u;
+	assert(1.7 == *u);
+
+
+	auto c = swap.cash();
+	assert(-1 == *c);
+	++c;
+	assert(1.5 == *c);
+	++c;
+	assert(1.5 == *c);
+	++c;
+	assert(1.5 == *c);
+	++c;
+	assert(1 == *c);
+
+
+	assert(std::pair(0.0, -1.0) == *swap);
+	++swap;
+	assert(std::pair(0.5, 1.5) == *swap);
+	++swap;
+	assert(std::pair(1.0, 1.5) == *swap);
+	++swap;
+	assert(std::pair(1.5, 1.5) == *swap);
+	++swap;
+	assert(std::pair(1.7, 1.0) == *swap);
+	++swap;
+	assert(!swap);
+
+	return 0;
+}
+int test_instrument_swap_int_int_int = test_instrument_swap<double, double, int>();
 int main()
 {
 	return 0;
